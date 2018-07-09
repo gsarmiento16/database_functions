74f415f309b3b8acc8d9ca9353c46b241a23b0cf
Guillermo Sarmiento <guillermosarmiento@gmail.com>
2018-07-09 11:39:05 +0200

    BulkInsert

--- BulkInsert/BulkInsert.sql
+++ BulkInsert/BulkInsert.sql
@@ -1,5 +1,21 @@
-use test;
+USE [test]
+GO
+
+DROP TABLE [dbo].[TxtRawFile]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[TxtRawFile](
+	[TextRow] [nvarchar](max) NOT NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
 
 BULK INSERT dbo.TxtRawFile
-FROM 'c:\myFile.TXT';  
+FROM 'c:\myFile.txt';  
 GO 
